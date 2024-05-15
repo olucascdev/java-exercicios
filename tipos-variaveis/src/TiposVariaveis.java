ba35a3999e7485cfd2f37279931745c69b5b8639
oLucasDev <lucasdevproject@gmail.com>
2024-05-15 09:38:20 -0300

    Sintaxe java DIO

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,19 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+
+        String meuNome = "Lucas";
+        double salarioMinimo = 2500;
+        Short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+        
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;  /* DECLARAR CONSTANTE*/
+
+
+    }
+}
